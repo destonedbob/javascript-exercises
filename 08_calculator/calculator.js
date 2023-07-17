677c04451af3b5dceb724ed7fd5c4af4840c6257
Deston Chung <destonchung@gmail.com>
2023-07-17 17:00:45 +0800

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (!array) return 0;
+
+  return array.reduce((total, currNum) => total + currNum, 0);
 };
 
 const multiply = function() {
-
+  return [...arguments].reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  console.log(Array.from(Array(num + 1).keys()).slice(1))
+  return Array.from(Array(num + 1).keys()).slice(1).reduce((total, num) => total* num, 1)
 };
 
 // Do not edit below this line
