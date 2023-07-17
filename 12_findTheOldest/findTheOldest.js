36d3e8a393c82a5941ec2a517542226398cb5938
Deston Chung <destonchung@gmail.com>
2023-07-17 19:13:26 +0800

    Pass first test of findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    if (people.length === 0) return -1;
 
+    let firstPerson = people[0]
+    let oldestPerson =  people.reduce((oldestPersonYet, currPerson) => {
+        let currPersonAge = currPerson.yearOfDeath - currPerson.yearOfBirth
+        let oldestPersonYetAge = oldestPersonYet.yearOfDeath - oldestPersonYet.yearOfBirth
+
+        console.log(currPerson.name);
+        console.log(currPersonAge);
+        console.log(oldestPersonYetAge);
+        console.log(oldestPersonYet.name);
+
+        if (currPersonAge > oldestPersonYetAge) {
+            return currPerson;
+        } else {
+            return oldestPersonYet;
+        }
+    }, firstPerson);
+    return oldestPerson;
 };
 
 // Do not edit below this line
