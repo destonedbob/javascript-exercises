75ce3fce48e1f2215a1894ea36117d596b32bdc9
Deston Chung <destonchung@gmail.com>
2023-07-17 18:30:46 +0800

    Complete 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    collection = {}
+    function helper_fib(num) {
+        if (num <= 0) return "OOPS";
+        if (num == 1 || num == 2) return 1;
 
+        let previous;
+        let previousprev;
+        if (!collection[num - 1]) {
+            previous = helper_fib(num - 1);
+            collection[num - 1] = previous;
+        } else {
+            previous = collection[num - 1];
+
+        }
+
+        if (!collection[num - 2]) {
+            previousprev = helper_fib(num - 2);
+            collection[num - 2] = previousprev;
+        } else {
+            previousprev = collection[num - 2];
+
+        }
+
+        return helper_fib(num - 1) + helper_fib(num - 2);
+    }
+    return helper_fib(Number.parseInt(num));
 };
 
 // Do not edit below this line
