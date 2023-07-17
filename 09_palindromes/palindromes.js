6556f385b0abb8a78cc03c879776f85ff35444af
Deston Chung <destonchung@gmail.com>
2023-07-17 18:19:22 +0800

    Complete 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    let letters = Array.from(phrase.toLowerCase()).filter(char => char.match(/[a-z0-9]/i))
+    let lettersLength = letters.length
+    if (lettersLength === 0) return false;
+    let firstHalf = letters.slice(0, Math.floor(lettersLength/2));
+    let secondHalf;
+    if (lettersLength % 2 == 0) {
+        secondHalf = letters.slice(Math.floor(lettersLength/2)).reverse();
+    } else {
+        secondHalf = letters.slice(Math.floor(lettersLength/2) + 1).reverse();
+    }
+    console.log(firstHalf)
+    console.log(secondHalf)
 
+    return firstHalf.every((ele, index) => ele == secondHalf[index]);
 };
 
 // Do not edit below this line
